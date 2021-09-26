2f0fe892558db277df1322e3385e405ba6aa450b
brexite <67023611+brexite@users.noreply.github.com>
2021-09-26 18:02:25 +0100

    Args fix

--- commands/tag.js
+++ commands/tag.js
@@ -11,13 +11,12 @@ module.exports = {
     //message.channel.send('https://cdn.glitch.com/48b7f59d-ed30-4a81-932f-c52986aa6b02%2F7daysfree.gif?v=1591405740033');
 
     if (args[0] == "add") {
+      image = args[1];
       var toAdd = {
-        img =  args[1],
+        img = image,
         author = message.author.id,
         created = Date.getTime()
       }
-
-      
       fs.readFile('../resources/tag.json', json, 'utf8', function readFileCallback(err, data){
         if (err){
             console.log(err);
