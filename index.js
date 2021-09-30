6a32b431aa34c540d3fcbededa12f8fdf91e1c6e
brexite <mblackmanwatts@gmail.com>
2021-09-30 09:22:23 +0000

    Fixed dms super quick aha

--- index.js
+++ index.js
@@ -64,8 +64,7 @@ bot.on('voiceStateUpdate', (oldState, newState) => {
 
 bot.on('messageCreate', message => {
 
-  if (message.channel.type === "dm") {
-    console.log(hi);
+  if (message.channel.type === "DM") {
     message.author.send(
       replyArray[Math.floor(Math.random() * replyArray.length)]
     );
