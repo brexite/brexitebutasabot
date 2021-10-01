3e38758635befb24f46bd5f69b74fbd1c511e6b5
brexite <mblackmanwatts@gmail.com>
2021-10-01 22:31:14 +0000

    Userinfo added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,16 +1,49 @@
-const fs = require("fs");
-const path = require("path");
-const serverPath = path.join(__dirname, "../assets/serverdata.json");
-const serverdata = require("../assets/serverdata.json");
-require('dotenv').config();
-
 module.exports = {
-  name: 'userinfo',
-  aliases: ["version"],
-  description: 'Gets information on the user, or a specified user',
-  usage: `userinfo`,
-  category: "Util",
+	name: "userinfo",
+  aliases: ['info','user','whois'],
+	description: "Returns information about the user",
+	usage: "userinfo",
+	category: "Util",
   execute: async (bot, message, args) => {
-    return message.channel.send("Command under construction, come back later.")
+    
+    const Discord = require('discord.js')
+    let config = require('../config.json')
+    
+    //emojis
+    const willoff = "<:willoff:738540171254956064>";
+    
+    let member = message.mentions.members.first() || message.member,
+    user = member.user;
+    
+    var botCheck
+    if (user.bot == true) {
+      botCheck = "✅"
+    } else {
+      botCheck = "❌"
+    }
+
+    var d = new Date(user.createdTimestamp)
+    d = d.toString().split(" GMT")[0]
+    
+    var e = new Date(member.joinedTimestamp)    
+    e = e.toString().split(" GMT")[0]
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle(user.tag)
+      .setColor(config.colour)
+      .setThumbnail(user.avatarURL({ dynamic:true }))
+      .setDescription([
+        `${willoff}` + "**Nickname:** "+ member.displayName,
+        `${willoff}` + "**ID:** `"+ member.id+"`",
+        `${willoff}` + "**Joined Guild:**  "+ e + " (UTC)",
+        `${willoff}` + "**Joined Discord:** " + d + " (UTC)",
+        "‎",
+        `${willoff}` + "**Bot:** " + botCheck,
+        `${willoff}` + "**Roles:** " + member.roles.cache.map(r => `${r}`).join(' | ')
+      ].join("\n"));
+    
+      embed.setTimestamp()
+      .setFooter(message.member.user.tag + " | " + message.guild.name, message.member.user.avatarURL({ dynamic:true }));
+    message.channel.send({embeds: [embed]});
   }
-}
+};
