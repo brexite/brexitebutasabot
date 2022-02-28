41188b19874c22a942eca8a796a21610b3b5519a
brexite <mblackmanwatts@gmail.com>
2022-02-28 14:15:10 +0000

    Removed error

--- commands/poke.js
+++ commands/poke.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
 	name: "poke",
 	description: "See what happens idiot",
-	usage: "poke <question>",
+	usage: "poke",
 	category: "Fun",
   execute: async (bot, message, args) => {
 
