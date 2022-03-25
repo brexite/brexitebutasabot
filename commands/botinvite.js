92f3f945c0f5a886b25177cff072216a88312d90
brexite <mblackmanwatts@gmail.com>
2022-03-25 15:33:32 +0000

    Fixed text formatting

--- commands/botinvite.js
+++ commands/botinvite.js
@@ -5,6 +5,6 @@ module.exports = {
     category: "Util",
     execute: async (bot, message, args) => {
         message.reply(`check dms x`);
-        message.author.send(`Add the bot to your server here! >> https://discord.com/api/oauth2/authorize?client_id=609326951592755211&permissions=1642787761399&scope=bot <<`);
+        message.author.send(`Add the bot to your server!\nhttps://discord.com/api/oauth2/authorize?client_id=609326951592755211&permissions=1642787761399&scope=bot`);
     }
 }
