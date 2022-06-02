8b20d6b634859824209874287e50a85cc668e4c9
brexite <mblackmanwatts@gmail.com>
2022-06-02 18:00:36 +0000

    imdoing and membercount changes

---
+++ commands/imdoing.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+const serverPath = path.join(__dirname, "../assets/serverdata.json");
+const serverData = require("../assets/serverdata.json");
+require('dotenv').config();
+
+module.exports = {
+  name: 'imdoing',
+  aliases: ["doing"],
+  description: `Enables or disables the "im doing x"/"im x" reply behaviour server-wide`,
+  usage: `imdoing <enable/disable/info>`,
+  category: "Admin",
+  args: true,
+  execute: async (bot, message, args) => {
+
+    if (args[0]) {
+
+      JSON.parse(fs.readFileSync(serverPath, "utf8"));
+      const action = args[0];
+
+      switch (action) {
+        case "enable":
+          serverData[message.guild.id].imDoingFeature = true;
+            fs.writeFile(
+              serverPath,
+              JSON.stringify(serverData, null, "\t"),
+              err => {
+                console.log(serverData);
+                console.error(err);
+              }
+            );
+            message.reply("I'm Doing Stuff is enabled!");
+          break;
+
+        case "disable":
+            serverData[message.guild.id].imDoingFeature = false;
+            fs.writeFile(
+              serverPath,
+              JSON.stringify(serverData, null, "\t"),
+              err => {
+                console.log(serverData);
+                console.error(err);
+              }
+            );
+            message.reply("I'm Doing Stuff is disabled!");
+          break;
+        case "info":
+              message.reply(`I'm Doing Stuff is currently ${serverData[message.guild.id].imDoingFeature ? "**Enabled**" : "**Disabled**"} for this server.`)
+          break;
+      }
+    }
+  }
+}
